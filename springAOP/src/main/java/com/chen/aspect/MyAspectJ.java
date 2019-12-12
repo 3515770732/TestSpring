fc1d4f1c75062d253c38b955f17c9dd4a880eb53
chen <3515770732@qq.com>
2019-12-12 10:50:32 +0800

    aop

--- springAOP/src/main/java/com/chen/aspect/MyAspectJ.java
+++ springAOP/src/main/java/com/chen/aspect/MyAspectJ.java
@@ -4,6 +4,8 @@ package com.chen.aspect;
  * @description:
  * @author:
  */
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -20,24 +22,37 @@ public class MyAspectJ {
     }
 
 
-    @Before("pointcut()")
+    //@Before("pointcut()")
     public void before(){
         System.err.println("aop----before");
     }
 
 
-    @After("pointcut()")
+    //@After("pointcut()")
     public void after(){
         System.err.println("aop----after");
     }
 
-    @AfterThrowing("pointcut()")
+    //@AfterThrowing("pointcut()")
     public void afterThrowing(){
         System.err.println("aop----afterThrowing");
     }
 
-    @AfterReturning("pointcut()")
+    //@AfterReturning("pointcut()")
     public void AfterReturning(){
         System.err.println("aop----afterReturning");
+    }
+
+    @Around("pointcut()")
+    public void Around(ProceedingJoinPoint point) throws Throwable {
+        System.err.println("ProceedingJoinPoint before around============"+point.getThis());
+        point.proceed();
+        System.err.println("ProceedingJoinPoint after around============="+point.getTarget());
+    }
+
+    @Around("pointcut()")
+    public void Around1(JoinPoint point) throws Throwable {
+        System.err.println("joinPoint before around======================"+point.getThis());
+        System.err.println("joinPoint after around======================="+point.getTarget());
     }
 }
