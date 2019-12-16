d7e51e1843ecb485a649c1b2b46cefd653cb1830
chen <3515770732@qq.com>
2019-12-16 17:13:37 +0800

    模拟spring

--- chenSpring/src/main/java/com/chen/ChenSpringConfig.java
+++ chenSpring/src/main/java/com/chen/ChenSpringConfig.java
@@ -1,8 +1,6 @@
 package com.chen;
 
-import com.chen.dao.IUserDao;
-import com.chen.dao.UserDaoImpl;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 
 /**
--- chenSpring/src/main/java/com/chen/service/UserServiceImpl.java
+++ chenSpring/src/main/java/com/chen/service/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package com.chen.service;
 
 import com.chen.dao.IUserDao;
-import com.chen.dao.UserDaoImpl;
 
 /**
  * @description:
